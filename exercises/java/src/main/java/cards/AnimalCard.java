fc6e9105b57018cbb5191f083d595cdbab129a02
Kit Ly <kit.ly@autotrader.co.uk>
2026-05-08 17:05:41 +0100

    Snap now interacts with Deck interface. Overrode equals to correctly compare instances of animal card in snap game

--- exercises/java/src/main/java/cards/AnimalCard.java
+++ exercises/java/src/main/java/cards/AnimalCard.java
@@ -1,5 +1,7 @@
 package cards;
 
+import java.util.Objects;
+
 public class AnimalCard implements Card {
 
     private final Animal animal;
@@ -15,5 +17,17 @@ public class AnimalCard implements Card {
     @Override
     public String toString() {
         return animal.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AnimalCard that = (AnimalCard) o;
+        return animal == that.animal;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(animal);
     }
 }
